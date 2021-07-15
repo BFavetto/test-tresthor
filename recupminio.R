bb6d553f4d521c37a54d112455b4257fbc49d020
benjaminfavetto <benjamin.favetto@dgtresor.gouv.fr>
2021-07-15 16:52:32 +0200

    Test avec S3

--- recupminio.R
+++ recupminio.R
@@ -1,4 +1,12 @@
 # récupération des données sur Minio
 
+# utilisation de S3
 library(aws.s3)
+
+# Informations sur le contenu du bucket
 mybucket <- aws.s3::get_bucket("benjaminfavetto", region = "")
+
+# Vérifie si l'objet est présent 
+head_object("tresthor_dev-main.tar.gz","benjaminfavetto", region= "")
+
+# get_object()
