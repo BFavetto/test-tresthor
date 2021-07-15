e381629fd64f18d9a5e6096f6a9afd7291c6a9a2
benjaminfavetto <benjamin.favetto@dgtresor.gouv.fr>
2021-07-15 17:58:33 +0200

    récupération du package correspondant à R 4.0.4

--- recupminio.R
+++ recupminio.R
@@ -9,8 +9,12 @@ mybucket <- aws.s3::get_bucket("benjaminfavetto", region = "")
 # Vérifie si l'objet est présent 
 # head_object("tresthor_dev-main.tar.gz","benjaminfavetto", region= "")
 
-object_exists("tresthor_dev-main.tar.gz","benjaminfavetto", region= "")
+object_exists("tresthor_1.0.0.tar.gz","benjaminfavetto", region= "")
 
 # Récupère l'objet et le sauvegarde
 # get_object("tresthor_dev-main.tar.gz","benjaminfavetto", region= "")
-save_object("tresthor_dev-main.tar.gz","benjaminfavetto", region= "")
+setwd("~/")
+save_object("tresthor_1.0.0.tar.gz","benjaminfavetto", region= "")
+
+# sauvegarde l'objet
+# put_object("tresthor_1.0.0.tar.gz","tresthor_1.0.0.tar.gz","benjaminfavetto", region= "")
